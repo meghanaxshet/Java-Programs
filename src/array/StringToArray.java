e9ede5c65ea9d4a65b2d3b6464b9731a30c4b2a0
RAIKAR <meghanashet97@gmail.com>
2024-07-07 19:45:23 +0530

    New commit

---
+++ src/array/StringToArray.java
@@ -0,0 +1,9 @@
+package array;
+
+public class StringToArray {
+    public static void main(String[] args) {
+        String string = "Megha";
+        char[] chars = string.toCharArray();
+        System.out.println("First element of the array : " +chars[0]);
+    }
+}
