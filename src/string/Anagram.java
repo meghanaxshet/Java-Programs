8ed0fe7fc91c7c2a1e316374e9c8303a91b0bfa7
RAIKAR <meghanashet97@gmail.com>
2024-07-09 19:01:08 +0530

    New commit

--- src/string/Anagram.java
+++ src/string/Anagram.java
@@ -4,23 +4,24 @@ import java.util.Arrays;
 
 public class Anagram {
 
-    public static void areAnagram(String s1, String s2) {
+    public static boolean areAnagram(String s1, String s2) {
         if (s1.length() != s2.length()) {
-            System.out.println(s1 + " " + "and" + " " + s2 + " " + "are not anagram");
+            return false;
         } else {
             char[] chars = s1.toCharArray();
             char[] chars1 = s2.toCharArray();
 
             Arrays.sort(chars);
             Arrays.sort(chars1);
-            Arrays.equals(chars, chars1);
-            System.out.println("Is Anagram");
+            return Arrays.equals(chars, chars1);
+
         }
     }
 
 
     public static void main(String[] args) {
-        Anagram.areAnagram("liste", "silent");
+        System.out.println(areAnagram("listen", "silent"));
+        System.out.println(areAnagram("unity", "unit"));
     }
 }
 
